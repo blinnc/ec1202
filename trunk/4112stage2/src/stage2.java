1728dbc78cc5c4ad039a01e93478c9f523a446c4
wangsean21@gmail.com <wangsean21@gmail.com@a9976af0-af02-7808-19d7-3259f6e344a6>
2011-04-11 04:14:48 +0000

    Implemented bitmap thingy

--- trunk/4112stage2/src/stage2.java
+++ trunk/4112stage2/src/stage2.java
@@ -9,11 +9,19 @@ import java.util.StringTokenizer;
 /**
  * Databases 4112 / Project 2 / Stage 2 Algorithm implementation
  * 
- * @author Jeff Sinckler/jcs2137
  * @author Sean Wang
+ * @author Jeff Sinckler/jcs2137
  *
  */
 public class stage2 {
+	
+	public static int arrayAccessCost;
+	public static int ifTestCost;
+	public static int logicalAndCost;
+	public static int bMispredictCost;
+	public static int arrayWriteCost;
+	public static int funcApplyCost;
+	public static int numFunctions;
 
 	public static double[] getCostList(int setNumber, double[] selectivities)
 	{
@@ -39,26 +47,60 @@ public class stage2 {
 		}*/
 		return null;
 	}
+	
+	public static ArrayList<BasicTermSet> getSetList(double[] selectivityArray) {
+		int numTerms = selectivityArray.length;
+		int numSets = (int)Math.pow(2, numTerms);
+		boolean[][] bitmap = new boolean[numTerms][numSets];
+		
+		/* Fill bitmap */
+		for(int i = 0; i < numTerms; i++) {
+			int skip = (int)Math.pow(2, numTerms - i - 1);
+			int count = skip;
+			boolean fill = false;
+			for(int j = 0; j < numSets; j++) {
+				if(count == 0) {
+					count = skip;
+					fill = !fill;
+				}
+				bitmap[i][j] = fill;
+				count--;				
+			}
+		}
+		
+		/* Fill setList */
+		ArrayList<BasicTermSet> setList = new ArrayList<BasicTermSet>();
+		
+		for(int j = 1; j < numSets; j++) {
+			ArrayList<Double> termList = new ArrayList<Double>();
+			
+			for(int i = 0; i < numTerms; i++) {
+				if(bitmap[i][j]) {
+					termList.add(new Double(selectivityArray[i]));
+				}
+			}
+			
+			double[] termArray = new double[termList.size()];
+			
+			for(int i = 0; i < termList.size(); i++) {
+				termArray[i] = termList.get(i);
+			}
+			
+			setList.add(new BasicTermSet(j, termArray));
+		}
+				
+		return setList;
+	}
+	
+	
+	
 	/**
 	 * @param args - takes the location of the query file as the first argument and the location
 	 * of the configuration file as the second query parameter
 	 */
-	public static void main(String[] args) {
-		
-		//algorithm cost values
-		int arrayAccessCost;
-		int ifTestCost;
-		int logicalAndCost;
-		int bMispredictCost;
-		int arrayWriteCost;
-		int funcApplyCost;
-		int numFunctions;
-		double[] funcSelectivities = new double[10];
-		
+	public static void main(String[] args) {		
 		//some helper data
 		String queryData;
-		ArrayList<BasicTermSet> basicTermSets = new ArrayList<BasicTermSet>();
-		int lineCounter = 0;
 		
 		//set up the properties file to read in the values for the algorithm
 		Properties properties = new Properties();
@@ -79,56 +121,38 @@ public class stage2 {
 		funcApplyCost = Integer.parseInt(properties.getProperty("f"));
 		
 		//Read in the query file
-		StringBuilder contents = new StringBuilder();
+		ArrayList<String> queryList = new ArrayList<String>();
 		try {
-		      //use buffering, reading one line at a time
-		      BufferedReader input =  new BufferedReader(new FileReader(args[0]));
-		      try {
-		        String line = null; //not declared within while loop
-		        while (( line = input.readLine()) != null){
-		          contents.append(line);
-		          contents.append("/");
-		          lineCounter++;
+			//use buffering, reading one line at a time
+			BufferedReader input =  new BufferedReader(new FileReader(args[0]));
+			try {
+				String line = null; //not declared within while loop
+				while (( line = input.readLine()) != null){
+		        	queryList.add(line);
 		        }
-		      }
-		      finally {
+			}
+			finally {
 		        input.close();
-		      }
+			}
     	}
 	    catch (IOException ex){
 	        ex.printStackTrace();
 	    }
-	    queryData = contents.toString();
 	    
 	    //get the first set of selectivities from the query file
-	    while(lineCounter >= 1)
-	    {
-		    //read the next set of values from the total list, then chop that portion of the list
-	    	//off of the string
-	    	String firstSet = queryData.substring(0, contents.indexOf("/"));
-		    queryData = queryData.substring(contents.indexOf("/") + 1, queryData.length());
+	    for(String query : queryList) {
 		    
 		    //write the selectivity values from the query into an array of doubles
-		    StringTokenizer st = new StringTokenizer(firstSet);
-		    int counter = 0;
-		    while(st.hasMoreTokens())
-		    {
-		    	funcSelectivities[counter] = Double.parseDouble(st.nextToken(" "));
-		    	counter++;
+		    String[] selectivityStrings = query.split(" ");
+		    double[] selectivities = new double[selectivityStrings.length];
+		    for(int i = 0; i < selectivityStrings.length; i++) {
+		    	selectivities[i] = Double.parseDouble(selectivityStrings[i]);
 		    }
-		    
-		    //set the total number of selectivities found in the line of the query
-		    numFunctions = counter;
 		    
 		    //loop that iterates over the selectivities and creates an extensive set. Needs work right now
 		    //TODO: Make this loop work...
-		    for(int i = 1; i <= numFunctions; i++)
-		    {
-		    	for(int j = i + 1; j <= numFunctions; j++)
-		    	{
-		    		basicTermSets.add(new BasicTermSet(i, funcSelectivities[j]));
-		    	}
-		    }
+		    
+		    ArrayList<BasicTermSet> basicTermSets = getSetList(selectivities);
 		    
 		    //iterate through the list of sets previously created and keep the cheapest cost
 		    for(int i = 0; i < basicTermSets.size(); i++)
@@ -164,14 +188,12 @@ public class stage2 {
 		    
 		    //print out the values (we may have to write to a file)
 		    System.out.println("====================");
-		    System.out.println(firstSet);
 		    System.out.println("--------------------");
 		    //TODO: print out the code for the algorithm
 		    System.out.println("--------------------");
 		    //TODO: print out the total cost from the big set
 		    System.out.println("Cost: " + 0.0);
 		    
-		    lineCounter--;
 	    }
 	}
 
